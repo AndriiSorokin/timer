3dcffcb37c3964b296a2be0db15fec4e38146fa3
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-25 11:49:57 +0200

    готово

---
+++ timer.js
@@ -0,0 +1,42 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector)
+    this.targetDate = targetDate.getTime()
+    this.day = document.querySelector('[ data-value="days"]');
+    this.hour = document.querySelector('[data-value="hours"]');
+    this.min = document.querySelector('[data-value="mins"]');
+    this.sec = document.querySelector('[data-value="secs"]')
+  }
+  timer() {
+    setInterval(() => {
+      let currentDate = new Date().getTime()
+      let time = this.targetDate - currentDate;
+      let days = Math.floor(time / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+      let secs = Math.floor((time % (1000 * 60)) / 1000);
+
+      this.day.textContent = `${days}`
+      this.hour.textContent = `${hours}`
+      this.min.textContent = `${mins}`
+      this.sec.textContent = `${secs}`
+
+      if (time < 0) {
+        clearInterval(this.expirationDate);
+        this.day.textContent = "00";
+        this.hour.textContent = "00";
+        this.min.textContent = "00";
+        this.sec.textContent = "00";
+      }
+    }, 1000);
+  }
+  init() {
+    this.timer();
+  }
+}
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Dec 31, 2021, 00:00"),
+}).init();
+
+
